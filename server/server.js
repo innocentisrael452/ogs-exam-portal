5880a30e4455b0fdbafeb7452e8d991b2a477ffd
innocentisrael452 <innocentisrael452@users.noreply.github.com>
2025-11-08 11:40:09 +0100

    Deploy frontend build

--- server/server.js
+++ server/server.js
@@ -21,8 +21,8 @@ app.use(bodyParser.json());
 // MongoDB connection
 const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/ogs_exam_portal";
 
-mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
+
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
